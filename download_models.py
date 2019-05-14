8019963e845ed6101b8d2c82e3104f185e778bc0
Jared Keown <jkeown@uvic.ca>
2019-05-14 14:26:39 -0700

    added model download script

---
+++ download_models.py
@@ -0,0 +1,22 @@
+import urllib2
+import tarfile
+
+print 'Downloading Models (2 GB)'
+
+filedata = urllib2.urlopen('http://www.astro.uvic.ca/~jkeown/astroclover/models.tar.gz')  
+datatowrite = filedata.read()
+
+with open('models.tar.gz', 'wb') as f:  
+    f.write(datatowrite)
+
+print 'Finished Downloading Models'
+
+###
+
+print 'Expanding Models'
+
+tar = tarfile.open("models.tar.gz")
+tar.extractall()
+tar.close()
+
+print 'Finished Expanding Models'
